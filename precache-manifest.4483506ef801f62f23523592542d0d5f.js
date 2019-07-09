4679621dd5ed4dcb8cd50704d04794ca2c84754a
wesley.williams@uk.ibm.com <welsey.williams@uk.ibm.com>
2019-07-09 11:48:05 +0100

    Updates

---
+++ precache-manifest.4483506ef801f62f23523592542d0d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c0c7012509d4c5d4318b6dd84fc49c",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4eabe4283dcf5a58cfb0",
+    "url": "/website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/website/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4eabe4283dcf5a58cfb0",
+    "url": "/website/static/js/main.aaa02f31.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
